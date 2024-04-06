92cd2ecf738c6c15945316019eb024ed0aae90d6
CRO102 <lamndps33323@fpt.edu.vn>
2024-04-06 13:13:34 +0700

    init project

--- index.js
+++ index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Login from './ASM/Login';
+import Main from './ASM/Main';
+import Sign from './ASM/Sign';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Main);
